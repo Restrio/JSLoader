9009f97bee9ef47245cd5cb1cdbc0fefc0f9df70
Tim Stumpenhusen <tim.stumpenhusen@gmx.de>
2016-12-05 23:03:00 +0100

    Added multiple configuration for one selector

--- assets/scripts/app/extensionLoader.js
+++ assets/scripts/app/extensionLoader.js
@@ -1,5 +1,5 @@
 define(["jquery"], function (jQuery) {
-  $j = jQuery.noConflict();
+  var $j = jQuery.noConflict();
 
   $j(document).ready(function() {
 
@@ -13,27 +13,65 @@ define(["jquery"], function (jQuery) {
     var extensionDependencies = {
       "#moveable": {
         "extensions": ["extensions/mainDemo"],
-        "callback": function() {
-          console.log("Main loaded");
+        "callback": function(jQuery, mainDemo) {
+          console.log(arguments)
         }
       },
-      ".notMain": {
+      "#myShit": [{
         "extensions": [],
         "callback": function() {
           console.log("notMain loaded");
         }
-      }
+      }, {
+        "extensions": [],
+        "callback": function() {
+          console.log("notMain loaded");
+        }
+      }]
     };
 
-    $j.each(extensionDependencies, function(selector, data) {
-      loadExtensionIfElementExists(selector, data.extensions, data.callback);
-    });
+    $j.each(extensionDependencies, parseDependencies);
 
-    // Loads Extension if specified selector gets at least 1 element
-    function loadExtensionIfElementExists(selector, extension, callback) {
-      if ($j(selector).length > 0) {
-        requirejs(extension, callback);
+    /**
+     *
+     * @param {string} selector - selector of markup-dependency
+     * @param {object|...object} data - contains (array of) following properties
+     * @param {...string} [data.extensions] - contains extensions to be loaded
+     * @param {function}  [data.callback] - contains callback-function
+     * @param {boolean} allowAbort
+     * @returns {boolean} - if markup-element has been found
+     */
+    function parseDependencies(selector, data, allowAbort) {
+      allowAbort = !!allowAbort;
+
+      // If Extension contains multiple extensions-configurations
+      if (Object.prototype.toString.call(data) === "[object Array]") {
+
+        // Load every single configuration-block by itself
+        $j.each(data, function(index, value) {
+          return parseDependencies(selector, value, true);
+        });
+
+      } else {
+        return loadExtensionIfElementExists(selector, data.extensions, data.callback, allowAbort);
       }
+    }
+
+    /**
+     * Loads Extension if specified selector gets at least 1 element
+     * @param selector - selector of markup-dependency
+     * @param extensions - extensions to be loaded
+     * @param callback - callback to be executed after successful load
+     * @param allowAbort - if script is allowed to tell if element exists
+     * @returns {boolean} - true (default) | false (markup-element does not exist)
+     */
+    function loadExtensionIfElementExists(selector, extensions, callback, allowAbort) {
+      callback = (typeof callback === "function" ? callback : undefined);
+      if ($j(selector).length > 0) {
+        requirejs(extensions, callback);
+        return true;
+      }
+      return !allowAbort;
     }
   });
 });
